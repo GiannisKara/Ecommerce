fb1984cc1cef9ee4d89ff24d05d839f6f081d269
karamas <99655065+karamas13@users.noreply.github.com>
2023-07-15 18:07:06 +0300

    Revert "carousel v2"
    This reverts commit fdca72428afcc3817f73970b3c9536c401e6fa72.

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,45 +1,27 @@
-import '@splidejs/react-splide/css/skyblue';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 import jordan1 from '../images/jordan1.png'
 import jordan2 from '../images/jordan2.png'
-import tshirt1 from '../images/t-shirt-1.png'
-import { Splide, SplideSlide, SplideTrack, Slide } from '@splidejs/react-splide';
 
-
-
-
-const CarouselComp = () => {  
-
-  const img = [
-    {
-      src: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/zzmfk3kwuw3ktkilxfnp/%CF%80%CE%B1%CF%80%CE%BF%CF%85%CF%84%CF%83%CE%B9%CE%B1-air-jordan-1-mid-83Lm6R.png"
-    },
-    {
-      src: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/zzmfk3kwuw3ktkilxfnp/%CF%80%CE%B1%CF%80%CE%BF%CF%85%CF%84%CF%83%CE%B9%CE%B1-air-jordan-1-mid-83Lm6R.png"
-    },
-    {
-      src: tshirt1
-    },
-];
-   
-  return (
-   <div> 
-     
-     <Splide options={ {
-    rewind: true,
-    gap   : '1rem',
-    hidden: false,
-    hasTrack: true
-  } } aria-label="My Favorite Images" className=" w-[30rem] h-auto relative ">
-     {img.map(image =>( 
-       <SplideSlide className="border border-violet-800 ">
-         <img src={image.src} />
-       </SplideSlide>
-    ))}
-    
-     </Splide>
-   
-    </div>
-  );
+const Carousel = () => {
+    return ( 
+      <Splide 
+      className='h-auto mx-auto w-[35rem]'
+      options={ {
+        rewind: true,
+        gap   : '1rem',
+      } }
+        
+      aria-label="My Favorite Images">        
+        <SplideSlide>
+          <img src={jordan2} alt="Image 2"/>
+        </SplideSlide>
+        <SplideSlide>
+          <img   src={jordan1} alt="Image 1"/>
+        </SplideSlide>
+        
+      </Splide>
+    );
 }
-
-export default CarouselComp;
+ 
+export default Carousel;
