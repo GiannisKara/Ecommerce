b2b242a6f82537788853d9309d57f7a1fa9225b7
karamas <99655065+karamas13@users.noreply.github.com>
2023-07-15 18:30:39 +0300

    fix commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Singup';
@@ -7,23 +6,10 @@ import Topnav from './layout/Topnav';
 import Footer from './layout/Footer';
 import Singleproduct from './pages/SingleProduct';
 import Carousel from './components/Carousel';
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Singup";
-import Home from "./pages/Home";
-import Topnav from "./layout/Topnav";
-import Footer from "./layout/Footer";
-import Singleproduct from "./pages/SingleProduct";
-import CartProvider from "./CartConstext";
-import Cancel from "./pages/Cancel";
-import Success from "./pages/Success";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
        <Router>
         <Topnav />
         <Routes>
@@ -36,26 +22,6 @@ function App() {
         </Routes>
        </Router>
        <Footer />
-=======
-      <CartProvider>
-        <Router>
-          <Topnav />
-          <Routes>
-            <Route exact path="/" />
-            <Route index element={<Home />} />
-            <Route
-              path="/pages/SingleProduct"
-              element={<Singleproduct />}
-            ></Route>
-            <Route path="/pages/Login" element={<Login />} />
-            <Route path="/pages/Signup" element={<Signup />} />
-            <Route path="Success" element={<Success />} />
-            <Route path="Cancel" element={<Cancel />} />
-          </Routes>
-        </Router>
-        <Footer />
-      </CartProvider>
->>>>>>> Stashed changes
     </div>
   );
 }
