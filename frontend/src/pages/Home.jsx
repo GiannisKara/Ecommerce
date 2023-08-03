a72717ac7a5cd83def6951fca0d1f4537354fcf3
karamas <99655065+karamas13@users.noreply.github.com>
2023-08-03 20:36:40 +0300

    Style tweaks and fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
 import { ProductArray } from "../ProductStore";
 import lotus from "../images/lotus.png";
- 
+import jeans from "../images/jeans.png"; 
+import tshirt from "../images/nike blue t-shirt.png";
+import shoes  from "../images/jordan1.png";
 
 const Home = () => {
   return (
@@ -13,21 +15,18 @@ const Home = () => {
           <p className="text-violet-50 mt-10 lg:text-[35px]">The superior E-commerce experience</p>
           </div>
           <h2 className="text-center text-violet-50 text-[30px] mt-12 mb-[-25px] lg:text-[55px] lg:mt-[150px]">What are you looking for?</h2>
-          <div className="flex flex-col justify-evenly m-10 h-[85rem] text-violet-50 lg:flex-row lg:mt-12">
-           <Link  to="./pages/TshirtList" className="flex flex-col items-center justify-center border-2 border-violet-50 m-5 h-[40%] text-[25px] rounded-xl lg:w-[18%] transition-ease-in-out delay-0 hover:transform hover:translate-x-4 hover:translate-y-4 duration-500">
-            <div>
-              <h2 className="">T-Shirts</h2>
-            </div>
+          <div  className="flex flex-col justify-evenly m-10 h-[85rem] text-violet-50 lg:flex-row lg:mt-12 ">
+           <Link to="./pages/TshirtList" className="relative flex flex-col items-center justify-center border-2 border-violet-50 m-5 h-[40%] text-[25px] rounded-xl lg:w-[25%] transition-ease-in-out delay-0 hover:transform hover:translate-x-4 hover:translate-y-4 duration-500">  
+             <h2 className="z-0 flex absolute text-violet-50 border w-full h-full items-center justify-center bg-violet-600 bg-opacity-40">T-Shirts</h2>
+             <img className="h-full w-full z-2 object-cover" src={tshirt}></img>
             </Link>
-            <Link to="./pages/JeansList" className="flex flex-col items-center justify-center border-2 border-violet-50 m-5 h-[40%] text-[25px] rounded-xl lg:w-[18%] transition-ease-in-out delay-0 hover:transform hover:translate-x-4 hover:translate-y-4 duration-500">
-            <div>
-              <h2>Jeans</h2>
-            </div>
+            <Link to="./pages/JeansList" className="relative flex flex-col items-center justify-center border-2 border-violet-50 m-5 h-[40%] text-[25px] rounded-xl lg:w-[25%] transition-ease-in-out delay-0 hover:transform hover:translate-x-4 hover:translate-y-4 duration-500">
+             <h2 className="z-0 flex absolute text-violet-50 border w-full h-full items-center justify-center bg-violet-600 bg-opacity-40">Jeans</h2>
+             <img className="h-full w-full z-2 object-cover" src={jeans}></img>
             </Link>
-            <Link to="./pages/ShoesList" className="flex flex-col items-center justify-center border-2 border-violet-50 m-5 h-[40%] text-[25px] rounded-xl  lg:w-[18%] transition-ease-in-out delay-0 hover:transform hover:translate-x-4 hover:translate-y-4 duration-500">
-            <div>
-              <h2>Shoes</h2>
-            </div>
+            <Link to="./pages/ShoesList" className="relative flex flex-col items-center justify-center border-2 border-violet-50 m-5 h-[40%] text-[25px] rounded-xl  lg:w-[25%] transition-ease-in-out delay-0 hover:transform hover:translate-x-4 hover:translate-y-4 duration-500">
+             <h2 className="z-0 flex absolute text-violet-50 border w-full h-full items-center justify-center bg-violet-600 bg-opacity-40">Shoes</h2>
+             <img className="h-full w-full z-2 object-cover" src={shoes}></img>
             </Link>
           </div>
     </div>   
